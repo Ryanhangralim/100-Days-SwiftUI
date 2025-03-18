fdfd3c545b83bde612fcd804b8a15b7073ede0b9
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-18 15:23:14 +0800

    Add blue title modifier

--- Project_3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project_3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,32 +7,24 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    let content: (Int, Int) -> Content
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.blue)
+    }
+}
 
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-            }
-        }
+extension View {
+    func blueTitleStyle() -> some View {
+        modifier(Title())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            HStack {
-                Image(systemName: "\(row * 4 + col).circle")
-                Text("\(row),\(col)")
-            }
-        }
+        Text("This should be a blue title")
+        .blueTitleStyle()
     }
 }
 
