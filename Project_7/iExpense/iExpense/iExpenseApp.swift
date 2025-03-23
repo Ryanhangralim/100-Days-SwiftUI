f8d46bfc5d820613093248212a57f55f368b64fe
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-23 12:29:20 +0800

    iExpense done

---
+++ Project_7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ryan Hangralim on 23/03/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
