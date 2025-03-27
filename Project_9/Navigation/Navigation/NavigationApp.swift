0391337e26f409e515368a39970630bc73901472
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-27 10:38:19 +0800

    Init project 9

---
+++ Project_9/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Ryan Hangralim on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
