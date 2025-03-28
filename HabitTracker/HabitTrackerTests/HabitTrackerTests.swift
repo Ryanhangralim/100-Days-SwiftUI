f1f0918995f073e379c872da5c74dd2c6e612902
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-29 07:54:02 +0800

    init habit tracker

---
+++ HabitTracker/HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Ryan Hangralim on 29/03/25.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
