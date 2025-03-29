465679d1cfe9bbf1b4c8b3f0b141e6d8a00fc3e0
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-29 08:16:41 +0800

    Added toolbar

--- HabitTracker/HabitTracker/ContentView.swift
+++ HabitTracker/HabitTracker/ContentView.swift
@@ -7,15 +7,71 @@
 
 import SwiftUI
 
+struct HabitItem: Identifiable, Codable {
+    var id = UUID()
+    let type: String
+    let description: String
+    let date: Date
+    let duration: Int
+}
+
+struct HabitType: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+}
+
+@Observable
+class Habits{
+    var items = [HabitItem](){
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    var types = [HabitType]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(types) {
+                UserDefaults.standard.set(encoded, forKey: "Types")
+            }
+        }
+    }
+    
+    init() {
+            if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+                if let decodedItems = try? JSONDecoder().decode([HabitItem].self, from: savedItems) {
+                    items = decodedItems
+                }
+            }
+            
+            if let savedTypes = UserDefaults.standard.data(forKey: "Types") {
+                if let decodedTypes = try? JSONDecoder().decode([HabitType].self, from: savedTypes) {
+                    types = decodedTypes
+                }
+            }
+        }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Text("Hello World")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: Text("Add activity view")) {
+                        Label("Add Activity", systemImage: "plus")
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarLeading){
+                    NavigationLink(destination: Text("View Habits")) {
+                        Text("View Habits")
+                    }
+                }
+            }
+            .navigationTitle("Habit Tracker")
         }
-        .padding()
     }
 }
 
