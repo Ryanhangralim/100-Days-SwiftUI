c19542890c2d82ee806dc1647bd53e757ad6d478
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-30 15:37:11 +0800

    init cupcake corner

---
+++ Project_10/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ryan Hangralim on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
