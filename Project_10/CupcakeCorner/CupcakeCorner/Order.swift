93986f5802fbcc2d4a3f5c8bbbec4c613bc852ed
Ryan Hangralim <ryanhangralim@gmail.com>
2025-03-31 13:11:33 +0800

    add first view of CupcakeCorner

---
+++ Project_10/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Ryan Hangralim on 31/03/25.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
