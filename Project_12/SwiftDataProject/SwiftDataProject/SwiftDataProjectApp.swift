e6cf736a04fb27e0596b330a1e0f7e07b303fd94
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-03 08:17:52 +0800

    Init SwiftDataProject and rename proejct 11

---
+++ Project_12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ryan Hangralim on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
