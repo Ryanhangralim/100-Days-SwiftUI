dd502e2bd9201dee456f6d990e5cb3e8952f4e82
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-03 09:26:52 +0800

    User view

--- Project_12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ Project_12/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Ryan Hangralim on 03/04/25.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
