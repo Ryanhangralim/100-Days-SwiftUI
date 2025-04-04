0a723e789747c66d827227b2113894047c959ad2
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-04 09:25:36 +0800

    Migrate class to struct and add fetch and parse data

--- FriendFace/FriendFace/FriendFace/ContentView.swift
+++ FriendFace/FriendFace/FriendFace/ContentView.swift
@@ -7,7 +7,31 @@
 
 import SwiftUI
 
+// Create data types
+struct Friend : Codable {
+    var id: String
+    var name: String
+}
+
+struct User: Codable {
+    var id: String
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+}
+
+
 struct ContentView: View {
+    // Variable to store friends data
+    @State private var users = [User]()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +40,39 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .task {
+            if(users.isEmpty){
+                print("Load data")
+                await loadData()
+            } else {
+                print("data exist")
+            }
+        }
+    }
+    
+    func loadData() async {
+        // Craft URL
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            return
+        }
+
+        // Fetch data
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            // decode data if valid
+            do {
+                let decodedUsers = try decoder.decode([User].self, from: data)
+                users = decodedUsers
+            } catch {
+                print("Decoding failed: \(error)")
+            }
+        } catch {
+            print("Failed to fetch data: \(error.localizedDescription)")
+        }
     }
 }
 
