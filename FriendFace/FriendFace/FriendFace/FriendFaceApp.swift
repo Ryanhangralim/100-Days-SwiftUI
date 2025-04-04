9f5c22680073bc783e602d48419551fdf3685910
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-04 08:56:09 +0800

    init FriendFace

---
+++ FriendFace/FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Ryan Hangralim on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FriendFace/FriendFace/FriendFaceTests/FriendFaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceTests.swift
+//  FriendFaceTests
+//
+//  Created by Ryan Hangralim on 04/04/25.
+//
+
+import Testing
+@testable import FriendFace
+
+struct FriendFaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
