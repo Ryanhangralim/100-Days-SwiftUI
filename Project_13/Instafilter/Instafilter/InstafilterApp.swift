301a254f05587385af2229b0cd392833adfe2d26
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-05 10:48:42 +0800

    Init Instafilter

---
+++ Project_13/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Ryan Hangralim on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
