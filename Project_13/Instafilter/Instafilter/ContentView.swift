966ce1e1aa27b4938c6215f55bc15c6cdca472a1
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-07 10:49:18 +0800

    Adding photopicker and loading image

--- Project_13/Instafilter/Instafilter/ContentView.swift
+++ Project_13/Instafilter/Instafilter/ContentView.swift
@@ -5,26 +5,32 @@
 //  Created by Ryan Hangralim on 05/04/25.
 //
 
-import StoreKit
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
+    @State private var selectedItem: PhotosPickerItem?
+    
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                // Image Area
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    // Image Area
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -49,6 +55,16 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            // loading data to UIImage when selectedItem changes (user uploaded image)
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // more code to come
+        }
     }
 }
 
