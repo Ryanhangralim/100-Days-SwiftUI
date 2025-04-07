41b8e321115cd097ae5a9c3d9690eaea89521b6e
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-07 10:43:53 +0800

    Build basic UI

--- Project_13/Instafilter/Instafilter/ContentView.swift
+++ Project_13/Instafilter/Instafilter/ContentView.swift
@@ -5,19 +5,50 @@
 //  Created by Ryan Hangralim on 05/04/25.
 //
 
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var commit = 0
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Button{
-                commit += 1
-            }label: {
-                Text("Commit count: \(commit)")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                // Image Area
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack{
+                    // Change filter
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // Share the picture
+                }
             }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
