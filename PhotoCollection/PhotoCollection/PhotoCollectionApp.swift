b9f9d6b7c40e7a69c154acd73d43ec8d8c328082
Ryan Hangralim <ryanhangralim@gmail.com>
2025-04-11 09:01:12 +0800

    Init PhotoCollection

---
+++ PhotoCollection/PhotoCollection/PhotoCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionApp.swift
+//  PhotoCollection
+//
+//  Created by Ryan Hangralim on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCollection/PhotoCollectionTests/PhotoCollectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionTests.swift
+//  PhotoCollectionTests
+//
+//  Created by Ryan Hangralim on 11/04/25.
+//
+
+import Testing
+@testable import PhotoCollection
+
+struct PhotoCollectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
